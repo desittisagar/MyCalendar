2b7e7889b44ffc6196180490ac6392a8712d231e
desittisagar <sagardesitti1410@gmail.com>
2018-11-13 21:21:02 +0530

    Initial commit

---
+++ events/models.py
@@ -0,0 +1,44 @@
+from __future__ import unicode_literals
+ 
+from django.db import models
+from django.urls import reverse
+from django.core.exceptions import ValidationError
+ 
+class Event(models.Model):
+    notes = models.TextField(max_length = 100)
+    day = models.DateField()
+    reminder = models.BooleanField(default = True)
+    start_time = models.TimeField(u'Starting time', help_text=u'Starting time')
+    end_time = models.TimeField(u'Final time', help_text=u'Final time')
+    
+ 
+    class Meta:
+        verbose_name = u'Scheduling'
+        verbose_name_plural = u'Scheduling'
+
+    def check_overlap(self, fixed_start, fixed_end, new_start, new_end):
+        overlap = False
+        if new_start == fixed_end or new_end == fixed_start:    #edge case
+            overlap = False
+        elif (new_start >= fixed_start and new_start <= fixed_end) or (new_end >= fixed_start and new_end <= fixed_end): #innner limits
+            overlap = True
+        elif new_start <= fixed_start and new_end >= fixed_end: #outter limits
+            overlap = True
+ 
+        return overlap
+ 
+    def get_absolute_url(self):
+        url = reverse('admin:%s_%s_change' % (self._meta.app_label, self._meta.model_name), args=[self.id])
+        return u'<a href="%s">%s</a>' % (url, str(self.notes))
+ 
+    def clean(self):
+        if self.end_time <= self.start_time:
+            raise ValidationError('Ending times must after starting times')
+ 
+        events = Event.objects.filter(day=self.day)
+        if events.exists():
+            for event in events:
+                if self.check_overlap(event.start_time, event.end_time, self.start_time, self.end_time):
+                    raise ValidationError(
+                        'There is an overlap with another event: ' + str(event.day) + ', ' + str(
+                            event.start_time) + '-' + str(event.end_time))
